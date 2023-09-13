45aa819820046518a535957a69d557ade148b6e1
m777ks <140451117+m777ks@users.noreply.github.com>
2023-09-13 20:51:14 +0300

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,35 @@
+import os
+import telebot
+
+hostname = "http://127.0.0.1:8000/"
+bot = telebot.TeleBot('6693909143:AAE3shto93QZbIL4zKB4B9N8naiDMhxW1KI')
+
+
+# response = os.system('ping ' + hostname)
+# bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start(message):
+  mess = f'Hello, <b>{message.from_user.first_name}</b>'
+  com = f'Введите сайт для пинга'
+  bot.send_message(message.chat.id, mess, parse_mode='html')
+  bot.send_message(message.chat.id, com, parse_mode='html')
+
+@bot.message_handler()
+def website(message):
+
+  response = os.system('ping ' + message.text)
+  bot.send_message(message.chat.id, response)
+  if response == 0:
+    bot.send_message(message.chat.id, f'{message.text} -- GOOD')
+  else:
+    bot.send_message(message.chat.id, f'{message.text} -- NOO')
+
+
+# response = os.system('ping ' + hostname)
+# bot = telebot.TeleBot(token)
+# if response == 0:
+#   print(hostname + ' is up!')
+# else:
+#   print(hostname + ' is down!')
+#   bot.send_message(message.chat.id, hostname + ' is down!')
+bot.polling(none_stop=True)
